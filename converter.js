73e9888fba75fdfd2b0a4f6989096c569be71bda
bappy <sohanbappy@gmail.com>
2020-02-24 15:52:26 +0600

    Protocol-22 Converted

--- converter.js
+++ converter.js
@@ -19,7 +19,7 @@ function bytesToHex(bytes) {
 let dataWithHwId = "0358979100015984787822221402160B2027C7028BBDF709B38C840014EF01D601523500B505010000000736970D0A";
 let bytes = [];
 //converter for String to Byte Array
-String.prototype.getBytes = function () {
+String.prototype.getBytes = function (){
     var bytes = [];
     for (var i = 0; i < this.length; ++i) {
       bytes.push(this.charCodeAt(i));
@@ -28,10 +28,11 @@ String.prototype.getBytes = function () {
   };
 
   //followed by Concox-JAVA (PacketreaderVR)
-  bytes = dataWithHwId.getBytes();
-  let raw_data = bytes.slice(8,bytes.length);
+  let h_id = dataWithHwId.slice(0,16);
+  let raw_data = dataWithHwId.slice(16,dataWithHwId.length);//withoutHID
+  console.log("HID: "+h_id);
   console.log("Raw Data: "+raw_data);
-  console.log(bytes[30]); //Byte  (ex-48) (In Java-> databytes[30])
+  console.log("Protocol: "+raw_data.slice(6,8));
   const convert = {
     bin2dec : s => parseInt(s, 2).toString(10),
     bin2hex : s => parseInt(s, 2).toString(16),
@@ -40,8 +41,25 @@ String.prototype.getBytes = function () {
     hex2bin : s => parseInt(s, 16).toString(2),
     hex2dec : s => parseInt(s, 16).toString(10)
   };
-  let hexValue = convert.dec2hex(bytes[30]);  //Hex (ex-30) (In Java-> String.format("%02X", databytes[30]))
-  console.log(hexValue);
+  //according to documentation
+console.log("Year: "+convert.hex2dec(raw_data.slice(8,10)));
+console.log("GPS: "+convert.hex2dec(raw_data.slice(21,22))); //1-bit(2nd) -> available GPS
+console.log("Lat: "+convert.hex2dec(raw_data.slice(22,30))/1800000); //decimal devided by 1800000
+console.log("Lon: "+convert.hex2dec(raw_data.slice(30,38))/1800000); //decimal devided by 1800000
+console.log("Speed: "+convert.hex2dec(raw_data.slice(38,40)));
 
+let course = convert.hex2bin(raw_data.slice(40,44));//ignoring prefix (0)
+if(course.length<16){
+  const diff = 16 - course.length; //find out missing zero(s)
+  for(let i=0;i<diff;i++) // adding Zero(s)
+  course='0'+course;
+}
+console.log("Total Course: "+course); //course and status
+console.log("Real-time or Differential? "+course[2]);
+console.log("GPS positioned or not? "+course[3]);
+console.log("East or West?"+course[4]);//Zero-East
+console.log("South or North? "+course[5]);//Zero-South
+console.log("Only Course: "+convert.bin2dec(course.slice(6,course.length))+" degree(s)"); //converting to decimal
 
-
+console.log("ACC: "+convert.hex2dec(raw_data.slice(60,62))); //After MCC,MNC,LAC,Cell-ID
+console.log("Mileage: "+convert.hex2dec(raw_data.slice(66,68))); //After Data Upload Mode,GPS Real-Time Re-upload
